1e159a08a4de4a151ef2bd8f4bb7fdd88665db4e
Saim Khan <93315183+KSAIM09@users.noreply.github.com>
2023-12-23 14:18:44 +0000

    New code

--- loginx-app/src/App.js
+++ loginx-app/src/App.js
@@ -1,70 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import './App.css';
+const App = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [showForm, setShowForm] = useState(true);
 
-const App = ()=>{
-  const [username, setUserName] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
-  // const [showForm, setShowForm] = useState(true);
-
-  const handleInputChange = (e) =>{
-    if(e.target.name === 'username'){
-      setUserName(e.target.value);
-    }else if (e.target.name === 'password'){
-      setPassword(e.target.value);
-    }
-  }
-
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (username.trim() === '' || password.trim() === '') {
-      setMessage('Please fill out both username and password fields.');
+    if (username === "user" && password === "password") {
+      setErrorMessage("");
+      setShowForm(false);
     } else {
-      if (username === 'user' && password === 'password') {
-        setMessage(`Welcome, ${username}!`);
-      } else {
-        setMessage('Invalid username or password');
-      }
+      setErrorMessage("Invalid username or password");
     }
   };
 
-
   return (
-    <div className='App'>
-      <form onSubmit={handleSubmit}>
-        <h1>Login Page</h1>
-        {message && <p>{message}</p>}
-        <lable htmlFor="Username">Username</lable>:
-        <input
-          placeholder='username'
-          type='text'
-          id='username'
-          name='username'
-          value={username}
-          onChange={handleInputChange}
-            // onChange={(e)=> setUserName(e.target.value)} 
-          required
-        />
-        <br />
-        <label htmlFor="Password">Password</label>:
-        <input
-          placeholder='password'
-          type='password'
-          id='password'
-          name='password'
-          value={password}
-          onChange={handleInputChange}
-            // onChange={(e)=> setPassword(e.target.value)}
-          required
-        />
-        <br />
-        <button type='submit'>Submit</button>
-      </form>
-    </div>
+    <>
+      <h1>Login Page</h1>
+      {showForm ? (
+        <form onSubmit={handleSubmit}>
+          {errorMessage && <div>{errorMessage}</div>}
+          <br />
+          <label>Username</label>:
+          <input
+            placeholder="username"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <br />
+          <label>Password</label>:
+          <input
+            placeholder="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      ) : (
+        <div>Welcome, user!</div>
+      )}
+    </>
   );
-
 };
 
 export default App;
